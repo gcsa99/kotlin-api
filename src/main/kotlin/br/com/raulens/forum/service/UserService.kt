52bfeb5b9b7a9b76631259f6971230aa104f489d
Guilherme Almeida <58594692+gcsa99@users.noreply.github.com>
2025-05-30 18:01:43 -0300

    feat: add user and course service

--- src/main/kotlin/br/com/raulens/forum/service/UserService.kt
+++ src/main/kotlin/br/com/raulens/forum/service/UserService.kt
@@ -1,2 +1,21 @@
-package br.com.raulens.forum.service 
+package br.com.raulens.forum.service
 
+import br.com.raulens.forum.model.User
+import org.springframework.stereotype.Service
+
+@Service
+class UserService(
+    var users: List<User>,
+) {
+    init {
+        val user = User(id = 1, name = "Typescripto de Spring React", email = "java@rust.go")
+        users = listOf(user)
+    }
+
+    fun findById(id: Long): User =
+        users
+            .stream()
+            .filter({ c -> c.id == id })
+            .findFirst()
+            .get()
+}
